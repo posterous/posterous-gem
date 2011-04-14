7a147cb6bd74aea7811482d64dfe35b1686df707
twoism <signalstatic@gmail.com>
2011-04-13 17:15:14 -0700

    cleanup post_spec

--- spec/unit/lib/postly/post_spec.rb
+++ spec/unit/lib/postly/post_spec.rb
@@ -4,32 +4,48 @@ describe Postly::Post do
   include Postly
 
   before(:all) do
-    @primary  = Site.find('primary')
-    @posts    = Post.all(@primary.id)
+    @primary = Site.find('postertester')
   end
 
-  it "should find the posts" do
-    @posts.should be_an Array
-  end
-
-  describe "creating and updating a post" do
+  describe "#all" do
     before(:all) do
-      @post = Post.create(@primary.id, {:title => 'from postly', :body => 'kittens are rad!'})
+      @posts = Post.all(@primary.id)
     end
 
-    it "should create a post" do
-      @post.title.should == 'from postly'
-    end
-
-    it "should update a post" do
-      @post.title = 'updated via postly'
-      @post.save
-      @post.reload.title.should  == 'updated via postly'
-    end
-
-    it "should delete a post" do
-      @post.destroy.should be_nil
+    it "finds the posts" do
+      @posts.should be_an Array
+      @posts.each{|p| p.id.should_not be_nil }
     end
   end
 
+  describe "CRUD" do
+    before do
+      @post     = Post.create(@primary.id, {:title => 'from postly', :body => 'kittens are rad!'})
+      @post_id  = @post.id
+    end
+
+    describe "#create" do
+      it "creates a post" do
+        @post.title.should == 'from postly'
+      end
+    end
+
+    describe "#update" do 
+      it "should update a post" do
+        @post.title = 'updated via postly'
+        @post.save
+        @post.reload.title.should  == 'updated via postly'
+      end
+    end
+
+    describe "#destroy" do
+      it "should delete a post" do
+        @post.destroy
+        lambda {
+          Post.find(@primary.id, @post.id)
+        }.should raise_error Postly::Connection::ConnectionError
+      end
+    end
+
+  end
 end
